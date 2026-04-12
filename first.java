b7a09f88700bc58714e56e0257c77c25ccf61065
devang-vyas-dev <builderwebsite94@gmail.com>
2026-04-12 17:47:15 +0530

    feat: Initial environment setup and First Code of Java

---
+++ first.java
@@ -0,0 +1,7 @@
+// This is the program foe printing the Hello World Message. Remember the Name of the class should 
+public class first{
+    public static void main(String[] args){
+        System.out.println("Hello World! My Java Journey has Oficially Started.");
+        System.out.println("My name is Devang Vyas and I am Lerning the Java Language.");
+    }
+} 
